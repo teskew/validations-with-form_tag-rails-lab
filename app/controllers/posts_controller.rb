6142c35af5642c71e5e086e29219a5950c65f688
teskew <teskinder1@gmail.com>
2021-04-05 22:16:49 -0500

    commit tree 463f4a72b86dcb8e6c5bd2b59ad53eaaa8ac1121

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,13 +7,34 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+ 
+
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+  @post =  Post.find(params[:id])
+  
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
+  
+  def new
+  @post = Post.new
+ end
+
+
+  def create
+    @post = Post.new(author_params)
+
+    if  @post.save
+     
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
+resources :authors, only: [:show, :new, :create]
+resources :posts, only: [:show, :edit, :update, :new, :create]
 end
